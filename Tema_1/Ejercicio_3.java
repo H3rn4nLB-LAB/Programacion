52fbcef03b9f7482022c3f4317aad444818c4cbb
H3rn4nLB-LAB <hernanlb06@gmail.com>
2026-09-21 13:44:19 +0200

    Add Ejercicio3 class to print personal information

---
+++ Tema_1/Ejercicio_3.java
@@ -0,0 +1,9 @@
+public class Ejercicio3 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hernan Lopez Bodes");
+    System.out.println("Calle Galaroza 28");
+		System.out.println("619 47 22 01");
+	}
+
+}
