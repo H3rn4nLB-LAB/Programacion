e67a77865f9646153d34519efd73db7ebf71e285
H3rn4nLB-LAB <hernanlb06@gmail.com>
2026-09-22 12:47:20 +0200

    Create Ejercicio_4.java

---
+++ Tema_1/Ejercicio_4.java
@@ -0,0 +1,18 @@
+public class Ejercicio4 {
+	
+	public static void main(String[] args) {
+		System.out.println("INGLES\tCASTELLANO");
+		System.out.println("------------------------");
+		System.out.println("Computer\tOrdenador");
+		System.out.println("Student\tEstudiante");
+		System.out.println("Classroom\tClase");
+	    System.out.println("Application\tAplicacion");
+	    System.out.println("Book\tLibro");
+	    System.out.println("Water\tAgua");
+	    System.out.println("Leaf\tHoja");
+        System.out.println("Restaurant\tRestaurante");
+        System.out.println("Beach\tPlaya");
+        System.out.println("Skyscraper\tRascacielos");
+	}
+
+}
