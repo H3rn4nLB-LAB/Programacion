df5061f3370dea15bd97fb6f1edc5b6ba2dd43fb
H3rn4nLB-LAB <hernanlb06@gmail.com>
2026-09-22 13:00:21 +0200

    Create Ejercicio_5.java

---
+++ Tema_1/Ejercicio_5.java
@@ -0,0 +1,16 @@
+public class Ejercicio5 {
+	
+	public static void main(String[] args) { 
+		System.out.println( "Lunes\tMartes\tMiercoles\tJueves\tViernes");
+		System.out.println("======\t=======\t======\t=======\t=======");
+		System.out.println("Lenguajes\tBD\tSistemas\tProgramacion\tED");
+		System.out.println("Lenguajes\tBD\tSistemas\tProgramacion\tBD"); 
+		System.out.println("Sistemas\tSostenibilidad\tItinerario\tLenguajes\tBD");
+		System.out.println("REC\tREC\tREC\tREC\tREC");
+		System.out.println("Sistemas\tED\tItinerario\tSistemas\tItinerario");
+		System.out.println("Programacion\tProgramacion\tDigit\tBD\tProgramacion");
+		System.out.println("Programacion\tProgramacion\tED\tBD\tProgramacion");
+	}
+
+}
+ 
